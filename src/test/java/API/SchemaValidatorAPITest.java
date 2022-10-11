e5bd9dcb210244c65e69c17648c78f9b9c467912
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-10-11 23:22:12 +0300

    add dependencies

---
+++ src/test/java/API/SchemaValidatorAPITest.java
@@ -0,0 +1,14 @@
+package API;
+
+import org.junit.Test;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+import io.restassured.*;
+
+public class SchemaValidatorAPITest {
+
+    @Test
+    public void givenUrl_whenJsonResponseConformsToSchema_thenCorrect() {
+    }
+}
