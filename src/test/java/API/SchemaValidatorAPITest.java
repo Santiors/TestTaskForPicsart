3118ca4e16a5b7e1174a4fdd5d6899fa394af208
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-10-12 00:17:39 +0300

    rebase and refactor json file

--- src/test/java/API/SchemaValidatorAPITest.java
+++ src/test/java/API/SchemaValidatorAPITest.java
@@ -1,15 +1,16 @@
 package API;
 
 import org.junit.Test;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
-import io.restassured.*;
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class SchemaValidatorAPITest {
 
     @Test
     public void givenUrl_whenJsonResponseConformsToSchema_thenCorrect() {
-
+        get("https://api.picsart.com/localizations/en/messages?project=reusable_components,photo_editor")
+                .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("get_component_editor.json"));
     }
 }
