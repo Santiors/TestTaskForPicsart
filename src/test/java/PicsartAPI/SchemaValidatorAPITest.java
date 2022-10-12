fba0f0232576ef7f68b04bf39bce97c858fc214f
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2022-10-12 13:54:08 +0300

    add log mechanism for api test

--- src/test/java/PicsartAPI/SchemaValidatorAPITest.java
+++ src/test/java/PicsartAPI/SchemaValidatorAPITest.java
@@ -1,16 +1,27 @@
 package PicsartAPI;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.junit.platform.engine.TestExecutionResult;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class SchemaValidatorAPITest {
 
+    public static Logger log = LogManager.getLogger();
+
     @Test
     public void givenRequest_should_match_JSONSchema_when_get_request() {
-        get("https://api.picsart.com/localizations/en/messages?project=reusable_components,photo_editor")
-                .then()
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("get_component_editor.json"));
+        try {
+            get("https://api.picsart.com/localizations/en/messages?project=reusable_components,photo_editor")
+                    .then()
+                    .assertThat()
+                    .body(matchesJsonSchemaInClasspath("get_component_editor.json"));
+            log.info(TestExecutionResult.successful());
+        } catch (Exception e) {
+            log.info(TestExecutionResult.failed(e));
+        }
     }
 }
